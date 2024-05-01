11990a7444c79a8b8c83b0b02eb120c7847c5980
cbastian <rodboianelli@gmail.com>
2024-05-01 20:11:40 -0300

    correccion

--- src/uploader.js
+++ src/uploader.js
@@ -3,7 +3,6 @@ import config from './config.js';
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        // cb(null, `${config.DIRNAME}/${config.UPLOAD_DIR}`)
         cb(null, config.UPLOAD_DIR)
     },
 
