bd8706a767d9eb332de8338b7c366c433a1d754c
cbastian <rodboianelli@gmail.com>
2024-05-01 06:24:04 -0300

    1ra-preentrega-corregida

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 import express from "express";
 import config from "./config.js";
+import productroutes from "./routes/products.routes.js"
+import cartroutes from "./routes/carts.routes.js"
+
 
 const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use("/api/products", productroutes);
+app.use("/api/carts", cartroutes);
 
+app.listen(config.PORT, () => {
+    console.log(`Servidor activo en puerto ${config.PORT}`);
+ 
+});
