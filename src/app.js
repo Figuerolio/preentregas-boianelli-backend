d18171b63a811623b91fdd89134106f9352b5388
cbastian <rodboianelli@gmail.com>
2024-05-05 03:00:00 -0300

    websockets

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import handlebars from "express-handlebars";
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
 import config from "./config.js";
 import productroutes from "./routes/products.routes.js"
 import cartroutes from "./routes/carts.routes.js"
@@ -16,12 +16,17 @@ const socketServer = new Server(httpServer);
 
 app.engine("handlebars",handlebars.engine());
 app.set("views",__dirname+"/views");
-app.set("view engine","handlebars");
+app.set("view engine","handlebars")
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use("/api/products", productroutes);
 app.use("/api/carts", cartroutes);
 
+
+
+socketServer.on("connection",socket=>{
+    console.log("cliente activo")
+})
 app.listen(config.PORT, () => {
     console.log(`Servidor activo en puerto ${config.PORT}`);
  
