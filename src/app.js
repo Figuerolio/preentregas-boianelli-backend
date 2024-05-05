fb33c2d77550a5fe61d5a91fd077bbf587571f3f
cbastian <rodboianelli@gmail.com>
2024-05-05 02:57:38 -0300

    websockets

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 import express from "express";
+import handlebars from "express-handlebars";
+import { Server } from "socket.io";
 import config from "./config.js";
 import productroutes from "./routes/products.routes.js"
 import cartroutes from "./routes/carts.routes.js"
 
 
 const app = express();
+const httpServer= app.listen(8080,()=>console.log("Escuchando"));
+const socketServer = new Server(httpServer);
 
+
+
+
+
+app.engine("handlebars",handlebars.engine());
+app.set("views",__dirname+"/views");
+app.set("view engine","handlebars");
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use("/api/products", productroutes);
